60d502b751e71ab42c4159f1ce7e73671ec88653
Yashika Jain <yashika.jain@vegascg.com>
2025-10-29 23:28:13 -0400

    Created draft of the prompt for the LLM

--- BST/hint_backend/utils/llm_client.py
+++
@@ -1,9 +0,0 @@
-"the llm function that will be used generating the hints"
-
-import google.generativeai as genai
-import os
-
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-def generate_hints(code_context, node_state, previous_hints, hint_number):
-    pass
---
+++ BST/hint_system/utils/prompt_builder.py
@@ -0,0 +1,34 @@
+"The prompt that the LLM will have, kept in a seperate file so it's able to be edited"
+
+
+concept = """
+def __contains__(self, item: Any) -> bool:
+    if self.is_empty():
+        return False
+    elif item == self._root:
+        return True
+    elif item < self._root:
+        return item in self._left
+    else:
+        return item in self._right
+bst1.__contains__(30)
+"""
+
+def llm_prompt(code_context, current_node, previous_hints):
+    # create history_context of all previous hints as a string 
+    return f"""
+You are a teaching assistant for CSC148. The student is learning about {concept}.
+
+Current step: {code_context}
+Highlighted node: {current_node}
+
+Prior hints so far:
+{history_context}
+
+Generate the next 5 progressive hints following this format:
+1. Prompt (reflective question)
+2. Reasoning (guiding logic)
+3. Explanation (conceptual summary)
+4. Connection (link to previous step)
+5. Next Step (suggest upcoming thinking)
+"""
